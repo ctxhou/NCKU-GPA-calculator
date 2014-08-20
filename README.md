4e220ea0247c7dba3674aac7db417e5f00fdf6a8
JackHou <vjack070707@gmail.com>
2014-08-21 02:17:56 +0800

    add

---
+++ README.md
@@ -0,0 +1,1 @@
+#NCKU GPA calculator
---
+++ js/ncku-gpa-calculator.js
@@ -0,0 +1,135 @@
+(function() {
+    // the minimum version of jQuery we want
+    var v = "1.3.2";
+
+    // check prior inclusion and version
+    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+        var done = false;
+        var script = document.createElement("script");
+        script.src = "https://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+        script.onload = script.onreadystatechange = function(){
+            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+                done = true;
+                initGPABookmarklet();
+            }
+        };
+        document.getElementsByTagName("head")[0].appendChild(script);
+    } else {
+        initGPABookmarklet();
+    }
+
+    function initGPABookmarklet() {
+        var gpaTotal = 0,
+            creditTotal = 0;
+
+        // get all the submit button name
+        var semesterNames = getSemesterName();
+        var allClass = []
+        // loop all the submit button
+        $.each(semesterNames, function(key, name){
+            var scoreAndCredit = []
+            var html = getSemesterHtml(name)
+            scoreAndCredit = analyzeSemesterGrade(html, semesterNames)
+            gpaTotal = gpaTotal + scoreAndCredit[0]
+            creditTotal = creditTotal + scoreAndCredit[1]
+            allClass.push(scoreAndCredit[2])
+        })
+        var gpaScore = (gpaTotal / creditTotal)
+        showResult(gpaScore, allClass, semesterNames)
+    }
+
+    function showResult(gpaScore, allClass, semesterNames){
+        //if it is not firefox, print the full result
+        if(!$.browser.mozilla){
+            var header = "<h3>Your GPA: "+ gpaScore + "</h3>"
+            var thead = "<tr><td>Class name</td><td>credit</td><td>score</td></tr>"
+            var tbody = ""
+            for (var key in allClass){
+                tbody = tbody + "<tr style='border-bottom: 1px solid white'><td style='padding-top: 20px; '>" + semesterNames[key] + "</td></tr>"
+                for(var detail in allClass[key]){
+                    tbody = tbody + "<tr><td>" + allClass[key][detail].className
+                                  + "</td><td>" + allClass[key][detail].credit
+                                  + "</td><td>" + allClass[key][detail].score + "</td></tr>"
+                }
+            }
+            $('body').append("<div id='my-score' style='padding: 10px;background-color:#f7f7f7; position:absolute; left: 25%; top: 0px'><button id='close'>close</button>"
+                                + header
+                                + "<table>"
+                                + thead
+                                + tbody
+                                + "</table> </div>")
+            $('#close').click(function(){
+                $('#my-score').remove();
+            })
+        }else{
+            alert("Your GPA: "+ gpaScore)
+        }
+    }
+
+    function getSemesterHtml(name) {
+        var html = null
+        $.ajax({
+            url: "?submit1="+name,
+            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+            async : false, /**/
+            processData : false,
+            type: "POST",
+            success: function(web){
+                html = web
+            }
+        })
+        return html
+    }
+
+    function analyzeSemesterGrade(html, semesterNames){
+        var gpaPart = 0;
+        var creditPart = 0;
+        var json = [];
+
+        html = html.replace(/(\/body|\/html)/i, "\/div")
+           .replace(/html/i, "div class='html'")
+           .replace(/body/i, "div class='body'");
+
+        $(html).find("table[bgcolor='#66CCFF'] tr:gt(1):not(:last)").each(function(){
+            var className = $(this).find('td:eq('+ 3 + ') b').html()
+            var credit = $(this).find('td:eq('+ 5 + ') b').html() //學分
+            var score = $(this).find('td:eq('+ 7 + ') b').html()  //分數
+            var origin = score
+            if (className !== null){
+                json.push({"className": className, "credit": credit, "score": score})
+            }
+            score = parseInt(score) || -1 //if the score is not appropriate, assign -1
+            if (score != -1) {
+                credit = parseInt(credit)
+                score = gpaScore(score);
+                gpaPart = gpaPart + score * credit
+                creditPart = creditPart + credit
+            }
+        })
+        return [gpaPart, creditPart, json]
+    }
+
+    function gpaScore(score){
+        var gpa = 0;
+        if(score >= 80){
+            gpa = 4;
+        }else if (score >= 70 && score <= 79){
+            gpa = 3;
+        }else if (score >= 60 && score <= 69){
+            gpa = 2;
+        }else if (score >= 50 && score <= 59){
+            gpa = 1;
+        }
+        return gpa
+    }
+    function getSemesterName() {
+        var semesterNames = [];
+        $("input:submit").each(function(){
+            var name = $(this).val()
+            semesterNames.push(name)
+        })
+        return semesterNames
+    }
+})();
+
+
